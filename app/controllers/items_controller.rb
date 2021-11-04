fb5f361596fbba952441de9735557778ec9fc049
+ <+>
2021-11-03 22:41:43 -0500

    commit tree 73bdab7002fb07efabae420a82f15bc9802e5359

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,5 @@
 class ItemsController < ApplicationController
+    def index
+        render json: Item.all, include: :user
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,5 @@
 class UsersController < ApplicationController
+    def show
+        render json: User.find(params[:id]), include: :items
+    end
 end
